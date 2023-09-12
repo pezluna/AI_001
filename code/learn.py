ede575affd11795da3ad6ced045e8909108ada79
pezluna <pezlunakr@gmail.com>
2023-09-12 22:16:35 +0900

    fix error

--- code/learn.py
+++ code/learn.py
@@ -98,9 +98,13 @@ def classify_using_random_forest(flows, labels, mode):
                         y.append(label[y_dict[mode]])
                         break
             else:
-                print(key.sid, key.did, key.protocol, key.additional)
-
-                exit(1)
+                if key.sid == '0x00000000' and key.did == '0x0000ffff':
+                    continue
+                elif key.sid == '0x0000ffff' and key.did == '0x00000000':
+                    continue
+                else:
+                    logger.error(f"Cannot find label for {key.sid}, {key.did}, {key.protocol}, {key.additional}")
+                    exit(1)
         
         X.append([data.delta_time, data.direction, data.length])
 
