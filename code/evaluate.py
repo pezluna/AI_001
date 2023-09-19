c15a8006ffd7311392c8364b923fc9664ac23b7f
pezluna <pezlunakr@gmail.com>
2023-09-20 06:09:59 +0900

    fix error

--- code/evaluate.py
+++ code/evaluate.py
@@ -67,7 +67,7 @@ def evaluate(test_flows, labels, mode, model_type, model):
         # if X.shape[0] % 4 != 0:
         #     logger.error(f"X's length is not divisible by 4: {X.shape[0]}")
         #     return
-        X = X.reshape(int(X.shape[0]) / 4, 4, 16)
+        X = X.reshape(int(X.shape[0]) // 4, 4, 16)
         y = y[::4]
         y = to_categorical(y, num_classes=len(np.unique(y)))
 
